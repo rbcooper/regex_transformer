819ca39ef78c97b5b9953f837f4c47e841496ebb
tkwa <kwathomas0@gmail.com>
2023-06-26 13:07:26 +0000

    add linear probe to parity_experiment

--- parity_experiment.py
+++ parity_experiment.py
@@ -15,7 +15,10 @@ import plotly.express as px
 import tqdm
 import liu_automata
 import dfa_generator
-
+from dfa_generator import DfaGenerator
+# sklearn logistic regression
+from sklearn.linear_model import LogisticRegression
+from einops import reduce, repeat, rearrange
 
 
 device = "cuda"
@@ -77,26 +80,39 @@ model = HookedTransformer(cfg)
 # %%
 
 # 2 can only appear after an even number of 0s
-gen = dfa_generator.DfaGenerator.from_regex('((1|2)*01*0)(1|2)*')
-batches, states = gen.get_batches_and_states(20, 10)
-batches
+gen = dfa_generator.DfaGenerator.from_regex('((B|C)*AB*A)*(B|C)*')
+data_loader = gen.batches_and_states_gen(word_len=20, batch_size=32)
+batch, states = data_loader.__next__()
+batch.shape, states.shape
+dfa_generator.display_fa(gen.dfa)
 # %%
+
+
+def loss_fn(logits, tokens, return_per_token=False):
+    # logit shape: [batch, pos, vocab]
+    # token shape: [batch, pos]
+    # assert False
+    logits = logits[:, :-1] # ignore last logit
+    tokens = tokens[:, 1:] # ignore first token (bos token)
+    log_probs = logits.log_softmax(-1)
+    correct_log_probs = log_probs.gather(-1, tokens[..., None])[..., 0]
+    loss = -correct_log_probs.mean() # mean over batch and pos
+    return loss
 
 
 # %%
 # aa_train = make_aa_generator(seed=123)
 # aa_test = make_aa_generator(seed=456)
 
-def train_basic_model(model, gen, batch_size=100, seed=123):
+def train_basic_model(model, gen:dfa_generator.DfaGenerator, batch_size=64, num_epochs=10_000, seed=123):
     print(f"Batch size: {batch_size}")
-    num_epochs = 20000
     lr = 1e-4
     betas = (0.9, 0.95)
     max_grad_norm = 1.0
     wd = 0.1
     optimizer = t.optim.AdamW(model.parameters(), lr=lr, betas=betas, weight_decay=wd)
     scheduler = t.optim.lr_scheduler.LambdaLR(optimizer, lambda i: min(i/100, 1.))
-    data_loader = gen(seed=seed,batch_size=batch_size, n_ctx=model.cfg.n_ctx)
+    data_loader = t.utils.data.DataLoader(gen.dataset(), batch_size=batch_size)
     print(data_loader)
 
     n_parameters = sum(p.numel() for p in model.parameters())
@@ -106,8 +122,8 @@ def train_basic_model(model, gen, batch_size=100, seed=123):
     """## Model Training"""
 
     losses = []
-    for epoch in tqdm.auto.tqdm(range(num_epochs)):
-        tokens = next(data_loader)
+    for epoch, (tokens, states) in tqdm.auto.tqdm(enumerate(data_loader), total=num_epochs):
+        # print(tokens.device, states.device)
         tokens = tokens.cuda()
         logits = model(tokens)
         loss = loss_fn(logits, tokens)
@@ -120,15 +136,20 @@ def train_basic_model(model, gen, batch_size=100, seed=123):
         losses.append(loss.item())
         if epoch % 100 == 0:
             print(f'Epoch {epoch}: {loss.item()}')
+        if epoch > num_epochs:
+            break
     return losses
 
 model = HookedTransformer(cfg)
-losses = train_basic_model(model, even_As_generator, batch_size=32, seed=123)
+# losses = train_basic_model(model, gen, batch_size=32, seed=123)
 # %%
-fig = px.line(losses, labels={"x":"Epoch", "y":"Loss"})
-fig.show()
+# fig = px.line(losses, labels={"x":"Epoch", "y":"Loss"})
+# fig.show()
 
 
+# Save model weights to file
+model.load_state_dict(t.load('model_weights.pt'))
+t.save(model.state_dict(), 'model_weights.pt')
 # %%
 
 def to_str(tokens):
@@ -152,27 +173,15 @@ fig.show()
 
 # %%
 
-# from tokenizers import Tokenizer
-# from tokenizers.models import CharacterLevel
-
-# tokenizer = Tokenizer(CharacterLevel())
-
 
 # %%
 
 
-tokens = even_As_generator(batch_size=10000, n_ctx=model.cfg.n_ctx).__next__()
-
+tokens, states = gen.batches_and_states_gen(batch_size=5000, word_len=model.cfg.n_ctx - 1).__next__()
+print(tokens.device)
 even_As_proportion = ((tokens == ord('A')).sum(axis=1) % 2 == 0).sum() / len(tokens)
 print(f"Fraction of even As in true dataset: {even_As_proportion}")
 # %%
-
-# playing around with automata file
-parity_automaton = automata.ParityAutomaton({'prob1': 0.5, 'length': 10})
-parity_automaton.sample() # returns a tuple (symbols, states)
-
-
-
 
 # %%
 
@@ -180,3 +189,28 @@ parity_automaton.sample() # returns a tuple (symbols, states)
 
 
 # First gather data: create a dataset, compute true states, and use model.run_with_cache to get hidden states
+with t.inference_mode():
+    model.eval()
+    logits, cache = model.run_with_cache(tokens)
+    print(f"logits is shape {logits.shape} with total {logits.numel()} elements")
+
+# %%
+# Now train a linear probe on states...
+assert states[(states != 1) & (states != 2)].flatten().sum() == 0
+
+# 0 = even, 1 = odd
+states_transformed = states - 1
+states_transformed = rearrange(states_transformed[:, 1:], 'b p -> (b p)').cpu().detach().numpy()
+
+for layer in range(model.cfg.n_layers):
+    for loc in ('mid', 'post'):
+        resid = cache['resid_'+loc, layer]
+        resid = rearrange(resid, 'b p d_model -> (b p) d_model')
+        resid = resid.cpu().detach().numpy()
+        # Use logistic regression to predict whether there are an even number of As
+        lr = LogisticRegression(max_iter=1000)
+        lr.fit(resid, states_transformed)
+
+        score = lr.score(resid, states_transformed)
+        print(f"Score for {layer} {loc}: {score}")
+# %%
