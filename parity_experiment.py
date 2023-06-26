2102e19ca4a4d093b434db2ba52cf3f5ac2ddd3d
tkwa <kwathomas0@gmail.com>
2023-06-26 20:50:15 +0000

    More parity experiment work in progress
    We haven't yet found circuits

--- parity_experiment.py
+++ parity_experiment.py
@@ -142,15 +142,23 @@ def train_basic_model(model, gen:dfa_generator.DfaGenerator, batch_size=64, num_
     return losses
 
 model = HookedTransformer(cfg)
-losses = train_basic_model(model, gen, batch_size=32, seed=123)
+
+
 # %%
-# fig = px.line(losses, labels={"x":"Epoch", "y":"Loss"})
-# fig.show()
+train = False
+from datetime import datetime
+timestamp = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+model_file_name = f'model_weights_parity_8l_192_{timestamp}.pt'
+model_file_name = "model_weights_parity_8l_192_2023_06_26-07_40_02_PM.pt"
 
+if train:
+    losses = train_basic_model(model, gen, batch_size=32, seed=123)
+    fig = px.line(losses, labels={"x":"Epoch", "y":"Loss"})
+    fig.show()
+    t.save(model.state_dict(), model_file_name)
+else:
+    model.load_state_dict(t.load(model_file_name))
 
-# Save model weights to file
-# model.load_state_dict(t.load('model_weights_parity_8l_192.pt'))
-t.save(model.state_dict(), 'model_weights_parity_8l_192.pt')
 # %%
 
 def to_str(tokens):
@@ -171,15 +179,9 @@ def percentage_accepted(prompt):
 
 print(f"Fraction of accepted completions: {percentage_accepted(t.zeros((1000,1), dtype=t.int64) + ord('B'))}")
 
-
-# %%
-
-
 tokens, states = gen.batches_and_states_gen(batch_size=1000, word_len=model.cfg.n_ctx).__next__()
 print(tokens.device)
-even_As_proportion = ((tokens == ord('A')).sum(axis=1) % 2 == 0).sum() / len(tokens)
-print(f"Fraction of even As in true dataset: {even_As_proportion}")
-# %%
+
 
 # %%
 
@@ -270,3 +272,55 @@ for string in generated_strings:
 fig = px.histogram(failure_positions)
 fig.show()
 # %%
+
+# Callum's graphing functions
+
+update_layout_set = {
+    "xaxis_range", "yaxis_range", "hovermode", "xaxis_title", "yaxis_title", "colorbar", "colorscale", "coloraxis", "title_x", "bargap", "bargroupgap", "xaxis_tickformat",
+    "yaxis_tickformat", "title_y", "legend_title_text", "xaxis_showgrid", "xaxis_gridwidth", "xaxis_gridcolor", "yaxis_showgrid", "yaxis_gridwidth", "yaxis_gridcolor",
+    "showlegend", "xaxis_tickmode", "yaxis_tickmode", "xaxis_tickangle", "yaxis_tickangle", "margin", "xaxis_visible", "yaxis_visible", "bargap", "bargroupgap"
+}
+
+def imshow(tensor, renderer=None, **kwargs):
+    kwargs_post = {k: v for k, v in kwargs.items() if k in update_layout_set}
+    kwargs_pre = {k: v for k, v in kwargs.items() if k not in update_layout_set}
+    facet_labels = kwargs_pre.pop("facet_labels", None)
+    border = kwargs_pre.pop("border", False)
+    if "color_continuous_scale" not in kwargs_pre:
+        kwargs_pre["color_continuous_scale"] = "RdBu"
+    if "margin" in kwargs_post and isinstance(kwargs_post["margin"], int):
+        kwargs_post["margin"] = dict.fromkeys(list("tblr"), kwargs_post["margin"])
+    fig = px.imshow(transformer_lens.utils.to_numpy(tensor), color_continuous_midpoint=0.0, **kwargs_pre)
+    if facet_labels:
+        for i, label in enumerate(facet_labels):
+            fig.layout.annotations[i]['text'] = label
+    if border:
+        fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+        fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+    # things like `xaxis_tickmode` should be applied to all subplots. This is super janky lol but I'm under time pressure
+    for setting in ["tickangle"]:
+      if f"xaxis_{setting}" in kwargs_post:
+          i = 2
+          while f"xaxis{i}" in fig["layout"]:
+            kwargs_post[f"xaxis{i}_{setting}"] = kwargs_post[f"xaxis_{setting}"]
+            i += 1
+    fig.update_layout(**kwargs_post)
+    fig.show(renderer=renderer)
+
+def hist(tensor, renderer=None, **kwargs):
+    kwargs_post = {k: v for k, v in kwargs.items() if k in update_layout_set}
+    kwargs_pre = {k: v for k, v in kwargs.items() if k not in update_layout_set}
+    names = kwargs_pre.pop("names", None)
+    if "barmode" not in kwargs_post:
+        kwargs_post["barmode"] = "overlay"
+    if "bargap" not in kwargs_post:
+        kwargs_post["bargap"] = 0.0
+    if "margin" in kwargs_post and isinstance(kwargs_post["margin"], int):
+        kwargs_post["margin"] = dict.fromkeys(list("tblr"), kwargs_post["margin"])
+    fig = px.histogram(x=tensor, **kwargs_pre).update_layout(**kwargs_post)
+    if names is not None:
+        for i in range(len(fig.data)):
+            fig.data[i]["name"] = names[i // 2]
+    fig.show(renderer)
+# %% 
+
