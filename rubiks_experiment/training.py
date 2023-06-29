c6739a20baa3aead57f9763921d434bbe7e85743
tkwa <kwathomas0@gmail.com>
2023-06-29 18:34:43 +0000

    Record color loss in cube training

--- rubiks_experiment/training.py
+++ rubiks_experiment/training.py
@@ -31,7 +31,7 @@ from transformer_lens import HookedTransformer
 
 
 
-# %%
+# %% refresh(), device, etc.
 
 
 def refresh():
@@ -40,14 +40,11 @@ def refresh():
 refresh()
 
 
-# %%
-
 device = "cuda"
-# %%
-transformer_lens.HookedTransformerConfig
+# %% unused
 
 # reference_model = HookedTransformer.from_pretrained("gelu-4l", fold_ln=False, center_unembed=False, center_writing_weights=False)
-# %%
+# %% Make model
 
 base_transformer_config = frozendict(
     {
@@ -58,9 +55,9 @@ base_transformer_config = frozendict(
         "checkpoint_index": None,
         "checkpoint_label_type": None,
         "checkpoint_value": None,
-        "d_head": 32,
-        "d_mlp": 768,
-        "d_model": 192,
+        "d_head": 48,
+        "d_mlp": 1024,
+        "d_model": 256,
         "d_vocab": 256,
         "d_vocab_out": 256,
         "device": "cuda",
@@ -72,10 +69,10 @@ base_transformer_config = frozendict(
         "init_weights": True,
         # 'initializer_range': 0.035355339059327376,
         "model_name": "regex-tester",
-        "n_ctx": 31,
+        "n_ctx": 126,
         "n_devices": 1,
-        "n_heads": 4,
-        "n_layers": 4,
+        "n_heads": 6,
+        "n_layers": 6,
         # 'n_params': 12582912,
         "normalization_type": "LN",
         "original_architecture": "neel",
@@ -106,16 +103,10 @@ base_transformer_config = frozendict(
 
 cfg = transformer_lens.HookedTransformerConfig(**base_transformer_config)
 
+cfg.n_params
 
 
-
-# %%
-
-model = HookedTransformer(cfg)
-
-# %%
-
-# %%
+# %% Define loss functions
 
 
 def loss_fn(logits, tokens, return_per_token=False):
@@ -129,6 +120,20 @@ def loss_fn(logits, tokens, return_per_token=False):
     loss = -correct_log_probs.mean()  # mean over batch and pos
     return loss
 
+def color_loss(logits, tokens):
+    logits = logits.clone()[:, :-1]  # ignore last logit
+    tokens = tokens.clone()[:, 1:]  # ignore first token (bos token)
+    # ignore every 5th token; it's a direction
+    mask = t.tensor([x % 5 != 0 for x in range(tokens.shape[1])], dtype=t.bool)
+    logits = logits[:, mask]
+    tokens = tokens[:, mask]
+    # print(logits.shape, tokens.shape)
+    # print(rubiks_generator.tokenizer.decode(list(tokens[0])))
+    log_probs = logits.log_softmax(-1)
+    correct_log_probs = log_probs.gather(-1, tokens[..., None])[..., 0]
+    loss = -correct_log_probs.mean()  # mean over batch and pos
+    return loss
+
 
 # %%
 # aa_train = make_aa_generator(seed=123)
@@ -137,35 +142,13 @@ def loss_fn(logits, tokens, return_per_token=False):
 model = HookedTransformer(cfg)
 
 
-def percentage_accepted(prompt=None, model=model):
-    return 1.0
-    if prompt is None:
-        prompt = t.zeros((1000, 1), dtype=t.int64) + ord("B")
-    print(f"{prompt.shape=}")
-    max_tokens = model.cfg.n_ctx - prompt.shape[1]
-    generated_tokens = model.generate(
-        input=prompt,
-        eos_token_id=0,
-        max_new_tokens=max_tokens,
-        do_sample=True,
-        temperature=1,
-    )
 
-    generated_strings = [
-        gen.detokenize(line).strip("\x00") for line in generated_tokens
-    ]
-    print(generated_strings[:10])
-    accepted = [gen.dfa.accepts_input(line) for line in generated_strings]
-    gen.pprint_dfa_trajectory(generated_strings[0])
-    return sum(accepted) / len(accepted)
-
-
-# %%
+# %% train model function
 def train_basic_model(
-    model, batch_size=64, num_epochs=10_000, seed=123
+    model, batch_size=64, num_epochs=10_000, seed=123, save_every=None
 ):
-    project_name = f"rubiks-ABC-wandb-develop"
-    with wandb.init(project=project_name, job_type="train") as run:
+    project_name = f"rubiks-world-representation"
+    with wandb.init(project=project_name, entity="alighnment", job_type="train") as run:
         
         print(f"Batch size: {batch_size}")
         lr = 1e-4
@@ -209,40 +192,52 @@ def train_basic_model(
                 loss=loss.item(),
             )
             if epoch % 100 == 0:
-                accept_frac = percentage_accepted(model=model)
-                log_dict["accept_frac"] = accept_frac
-                print(f"Epoch {epoch}: {loss.item()} {accept_frac=}")
+                this_color_loss = color_loss(logits, tokens)
+                log_dict["color_loss"] = this_color_loss.item()
+                print(f"Epoch {epoch}: {loss.item()} {this_color_loss=}")
+            if save_every is not None and epoch % save_every == 0:
+                timestamp = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+                model_file_name = f"checkpoint_rubiks_{cfg.n_layers}l_{cfg.d_model}d_{cfg.n_ctx}c_{epoch}_{timestamp}.pt"
+                t.save(model.state_dict(), model_file_name)
             run.log(log_dict)
             if epoch > num_epochs:
                 break
         return losses
 
+# %% train model 
+
+if __name__ == "__main__":
+    train = True
+
+    timestamp = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+    model_file_name = f"model_weights_rubiks_{cfg.n_layers}l_{cfg.d_model}_{timestamp}.pt"
+
+    if train:
+        losses = train_basic_model(
+            model, batch_size=32, num_epochs=100_000, seed=123, save_every=10000
+        )
+        fig = px.line(losses, labels={"x": "Epoch", "y": "Loss"})
+        fig.show()
+        t.save(model.state_dict(), model_file_name)
+    else:
+        model_file_name = "/home/ubuntu/regex_transformer/rubiks_experiment/checkpoint_rubiks_6l_256d_126c_300000_2023_06_27-11_08_17_PM.pt"
+        model.load_state_dict(t.load(model_file_name))
 # %%
-train = True
 
-timestamp = datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
-model_file_name = f"model_weights_parity_{cfg.n_layers}l_{cfg.d_model}_{timestamp}.pt"
+if __name__ == "__main__":
+    with t.inference_mode():
+        model.eval()
+        dl = CubieRepresentation.dataloader(cfg.n_ctx, batch_size=32)
 
-if train:
-    losses = train_basic_model(
-        model, batch_size=32, num_epochs=30000, seed=123
-    )
-    fig = px.line(losses, labels={"x": "Epoch", "y": "Loss"})
-    fig.show()
-    t.save(model.state_dict(), model_file_name)
-else:
-    model_file_name = "model_weights_parity_8l_192_2023_06_26-07_40_02_PM.pt"
-    model.load_state_dict(t.load(model_file_name))
-# %%
-
-# with t.inference_mode():
-#     model.eval()
-#     dl = CubieRepresentation.dataloader(cfg.n_ctx, batch_size=31)
-
-#     for tokens, states in dl:
-#         print(tokens.shape)
-#         logits = model(tokens.cuda())
-#         logitslist(logits.argmax(-1))
-#         print(rubiks_generator.tokenizer.decode_batch()
-#         break
+        for tokens, states in dl:
+            tokens = tokens.cuda()
+            print(tokens.shape)
+            print(rubiks_generator.tokenizer.decode(list(tokens[0])))
+            logits = model(tokens)
+            this_color_loss = color_loss(logits, tokens).item()
+            print(f"{this_color_loss=:.3f}")
+            most_likely_tokens = list(logits.argmax(-1)[0])
+            s = rubiks_generator.tokenizer.decode(most_likely_tokens)
+            print(s)
+            break
 # %%
